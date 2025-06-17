25ccaf3f6866422cde5c53d7456dd424d7c12657
Ime Asamudo <iasamudo@tektelic.com>
2025-06-17 14:28:47 -0600

    Added assembly template

---
+++ Software/src/cm0dsasm.s
@@ -0,0 +1,84 @@
+;------------------------------------------------------------------------------------------------------
+; Design and Implementation of an AHB timer, a GPIO peripheral, and a 7-segment display peripheral  
+; 1)Display text string: "TEST" on VGA.
+; 2)Input data from switches and output them to LEDs;
+; 3)Display the timer value to the 7-segment display.
+;------------------------------------------------------------------------------------------------------
+
+
+; Vector Table Mapped to Address 0 at Reset
+
+						PRESERVE8
+                		THUMB
+
+        				AREA	RESET, DATA, READONLY	  			; First 32 WORDS is VECTOR TABLE
+        				EXPORT 	__Vectors
+					
+__Vectors		    	DCD		0x00003FFC
+        				DCD		Reset_Handler
+        				DCD		0  			
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD 	0
+        				DCD		0
+        				DCD		0
+        				DCD 	0
+        				DCD		0
+        				
+        				; External Interrupts
+						        				
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+              
+                AREA |.text|, CODE, READONLY
+;Reset Handler
+Reset_Handler   PROC
+                GLOBAL Reset_Handler
+                ENTRY
+
+				;Write "TEST" to the text console and the UART
+
+
+
+
+				;Configure the timer
+				
+
+
+AGAIN			
+				;Read from switch, and output to LEDs
+				
+
+				
+
+				;Read the current timer value, and output to 7-segments
+				
+	
+
+
+				ENDP
+
+				ALIGN 		4					 ; Align to a word boundary
+
+		END                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+   
